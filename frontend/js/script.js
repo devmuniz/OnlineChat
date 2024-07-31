106f2aa08fb6219e1b1e34ee77749a528d9e9a17
Gustavo Muniz <gustavomunizdr@hotmail.com>
2024-07-30 21:24:35 -0300

    Change server url

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -85,7 +85,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://onlinechat-backend-bypx.onrender.com")
     websocket.onmessage = processMessage
 }
 
