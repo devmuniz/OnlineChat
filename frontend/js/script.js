698dbd2a9884688c763dc3833de99a91d5c05c96
Gustavo Muniz <gustavomunizdr@hotmail.com>
2024-08-01 21:20:16 -0300

    URL server Att.

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -85,7 +85,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("wss://online-chat-chi.vercel.app/")
+    websocket = new WebSocket("ws://localhost:8080")
     websocket.onmessage = processMessage
 }
 
